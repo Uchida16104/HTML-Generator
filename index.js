004479cd7d418d8d02714c478d1f5f4a0a81eb6a
HU <uho02741358@gmail.com>
2023-08-03 20:56:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function composeElement(parentTag, index, childTag, type, name, cssText, input) {
     var element = document.createElement(childTag);
     element.setAttribute(type, name);
+    if (parentTag != "html" || parentTag != "head" || childTag != "script" || childTag != "style") {
     element.style.cssText = cssText;
+    }
     element.textContent = input;
     var parent = document.getElementsByTagName(parentTag)[index]
     parent.appendChild(element);
